3d746463e5aed1d20bd62ff1c8a10a30e3ce650a
root <root@localhost.localdomain>
2026-06-05 13:38:44 +0200

    chore: ignore local artifacts and helper scripts

--- start_backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pkill -f uvicorn
-proot-distro login ubuntu --shared-tmp -- bash -c "
-  source /root/venv/bin/activate &&
-  cd /data/data/com.termux/files/home/dev/github/mine/EU_Funds_and_Grants_AI &&
-  uvicorn main:app --reload --host 0.0.0.0 --port 8000
-"
-
